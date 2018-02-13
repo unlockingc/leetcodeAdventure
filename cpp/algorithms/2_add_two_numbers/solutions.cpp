397ea08fd8e723f4cc0b6eba642b551e488e4662
123malin <2660180796@qq.com>
2018-02-14 00:31:26 +0800

    2_add_two_numbers

--- cpp/algorithms/2_add_two_numbers/solutions.cpp
+++ cpp/algorithms/2_add_two_numbers/solutions.cpp
@@ -9,6 +9,25 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* root = new ListNode(0);
+        int temp_sum;
+        int carry = 0;
 
+        ListNode* ans = root;
+        while( l1 || l2 || carry ) {
+            temp_sum = carry;
+            if (l1) {
+                temp_sum += l1 -> val;
+                l1 = l1 -> next;
+            }
+            if (l2) {
+                temp_sum += l2 -> val;
+                l2 = l2 -> next;
+            }
+            ans -> next = new ListNode(temp_sum % 10);
+            ans = ans -> next;
+            carry = temp_sum / 10;
+        }
+        return root -> next;
     }
 };
